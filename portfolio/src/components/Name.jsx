f947ae20e1838a44a35ac5f7317cafa17cd443a0
HumphreyJ <joshhumphrey1@gmail.com>
2020-08-01 00:33:57 -0500

    made some sweet styling progress.

--- portfolio/src/components/Name.jsx
+++ portfolio/src/components/Name.jsx
@@ -6,7 +6,7 @@ const Name = () => {
     <div className='info'>
         <h4 className="name">Joshua Humphrey</h4>
         <h4 className="title">Front-End Developer</h4>
-        <h4 className="location">Texas,United States</h4>
+        <h4 className="location">Texas, United States</h4>
         <div className="git-in">
             <a href='https://www.linkedin.com/in/joshahumphrey/'
                 className='social-link'><i className=" nav fab fa-linkedin fa-2x"></i></a>
