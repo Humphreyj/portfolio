3bcdfb5a623125d116bfd0e1fb9a00fee43df49f
HumphreyJ <joshhumphrey1@gmail.com>
2020-08-03 17:21:48 -0500

    updated projects on the site.

--- portfolio/src/components/projects/Project.jsx
+++ portfolio/src/components/projects/Project.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import lander from '../../img/lander.png';
 
-const Project = () => {
+
+const Project = (props) => {
     return (
         <div className='project'>
-				<img className='project-img' src={lander} alt='project 2'/>
+				<img className='project-img' src={props.img} alt='project 2'/>
 				<div className="project-info">
-                    <p className="project-title">Dynamic Home Page</p>
-					<p className="tech-stack">HTML, CSS, JavaScript</p>
+                    <p className="project-title">{props.name}</p>
+					<p className="tech-stack">{props.stack}</p>
 					<p className='project-desc'>
-					The Front End for a 'Pirate Radio' station. I was tasked with the redesign of the site and implementation of additional features (WIP)
+					{props.description}
 					</p>
 					<div className="proj-buttons">
-						<a href='https://dynamiclander.netlify.app' className='proj-link '>View Site</a>
-						<a href='https://github.com/Humphreyj/pirate-radio/tree/master/outpost-radio' className='proj-link '>View Code</a>
+						<a href={props.site} target='_blank' className='proj-link '>View Site</a>
+						<a href={props.git} target='_blank' className='proj-link '>View Code</a>
 
 					</div>{/*Project buttons*/}
 				</div>
--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -2,6 +2,9 @@ import React,{useState,useContext} from 'react';
 import Project from'./Project';
 import Name from '../Name';
 import UIC from '../../context/UIC';
+import lander from '../../img/lander.png';
+import localMarket from '../../img/localmarket.png';
+import nasa from '../../img/nasa.png';
 
 const Projects = () => {
 const {showSkills, toggleSkills} =useContext(UIC);
@@ -12,9 +15,31 @@ const {showSkills, toggleSkills} =useContext(UIC);
                 <p onClick={toggleSkills}>Skills</p>
             </div>
             <h1 className="projects-title">Recent Projects</h1>
-            <Project />
-            <Project />
-            <Project />
+            <Project
+            img={lander}
+            name="Dynamic Home Page"
+            stack="HTML, CSS, JavaScript"
+            description="A homepage that retrieves the user's locations and returns the local weather. Also diplays the current price of Bitcoin and Ethereum using the CoinMarketCap API. User can set their name and current goal for the day."
+            site='https://dynamiclander.netlify.app/'
+            git='https://github.com/Humphreyj/dynamic-lander'
+             />
+            <Project
+            img={localMarket}
+            name="Local Market"
+            stack="JavaScript,React, Sass"
+            description="The Front End for an online grocery store. Users can navigate through items and place orders. Administrator can add and edit items in their store."
+            site='https://localmarket.netlify.app/'
+            git='https://github.com/Humphreyj/to-go'
+             />
+            <Project
+            img={nasa}
+            name="Nasa Photo of the Day"
+            stack="JavaScript,React, Styled Components"
+            description="A web application that consumes NASA's Photo of the Day API,and returns the weather data on Mars if it is available."
+            site='https://spaceyall.netlify.app/'
+            git='https://github.com/Humphreyj/nasa-photo-of-the-day/tree/josh-humphrey'
+            
+             />
         </div>
     );
 }
